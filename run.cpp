52054c5a5c7d9772334491bb7648ac81e9f075c8
Michael Hayes <Michael.hayes.2013@live.rhul.ac.uk>
2016-09-11 14:21:09 +0100

    Add files via upload

--- run.cpp
+++ run.cpp
@@ -1,8 +1,15 @@
 #include"core/GloveController.h"
 
 int main(){
-	std::vector<std::string> topicsFilepath = {"topics/physics.txt","topics/biology.txt",
-				"topics/chemistry.txt","topics/biochemistry.txt","topics/names.txt"};
-	GloveController glove("glove.6B/glove.6B.50d.txt",topicsFilepath,50);
-	glove.run();
+	//std::vector<std::string> topicsFilepath = {"topics/physics.txt","topics/biology.txt",
+//				"topics/chemistry.txt","topics/biochemistry.txt","topics/names.txt"};
+	GloveController glove("glove.6B/glove.6B.50d.txt",50);
+	glove.runQuery("king");
+	glove.runQuery("man");
+	glove.runQuery("woman");
+	glove.runQuery("queen");
+	glove.runQuery("maths");
+	glove.runQuery("physics");
+	glove.runQuery("chemistry");
+	glove.runQuery("biology");
 }
